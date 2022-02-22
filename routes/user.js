b2aac07cfcf382b32a72903c6cf0d10b69d6a505
AIT SI SAID Ahmed <35258553+AHMED2083@users.noreply.github.com>
2022-02-22 14:15:06 +0100

    insert login

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../controller/user.js";
+import { createUser, loginUser } from "../controller/user.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", createUser);
+userRoute.post("/login, loginUser);
 
 export default userRouter;
-
-
-
